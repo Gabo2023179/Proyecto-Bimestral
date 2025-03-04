c8c7197e6daf8aa04530cbcadf02f9d60b05ed7c
Gabo2023179 <jcontreras@kinal.edu.gt>
2025-03-03 23:34:20 -0600

    add: agregando funcionalidades al carrito de compra

--- src/cart/cart.routes.js
+++ src/cart/cart.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCart, addToCart, removeFromCart } from "../controllers/cart.controller.js";
+import { getCart, addToCart, removeFromCart, clearCart } from "../controllers/cart.controller.js";
 import { createCartItemValidator } from "../middlewares/cart.validators.js";
 import { validateJWT } from "../middlewares/validate-jwt.js";
 
@@ -8,5 +8,7 @@ const router = Router();
 router.get("/", validateJWT, getCart);
 router.post("/", validateJWT, createCartItemValidator, addToCart);
 router.delete("/", validateJWT, removeFromCart);
+// Endpoint adicional para vaciar el carrito
+router.delete("/clear", validateJWT, clearCart);
 
 export default router;
